70c21eb3742bae88741fe314b4b285e8c293f042
brodstar <brodie.woody@gmail.com>
2015-10-21 19:29:24 +1000

    Addition of SendMessage() class. This can be used to send messaged from the main service now.

--- server-component/bin/alert.py
+++ server-component/bin/alert.py
@@ -40,30 +40,32 @@ class MyHandler(EventHandler):
 
 logging.basicConfig(level = logging.INFO)  # change to 'DEBUG' to see more
 
-your_jid = settings_file.users['system']['secsys']
-your_password = getpass("Your password: ")  # [0] Secure method required to supply pass in an automated way
-# target_jid = settings.users['administrators']['keags']
-# [0] Require a solution to automate the way this message gets generated to specify the following
-#        - Secsys Alert: Sensor [0-10] Has been tripped
-#        - Secsys Alert: Workshop has been entered by authorized user
-#        - Secsys Alert: Failed to communicate with System for more than x(time)
-message = 'Secsys Alert: This is a test dev message'
 
-for admin in settings_file.users['administrators']:
-    target_jid = settings_file.users['administrators'][admin]
-    if sys.version_info.major < 3:
-        your_jid = your_jid.decode("utf-8")
-        your_password = your_password.decode("utf-8")
-        target_jid = target_jid.decode("utf-8")
-        message = message.decode("utf-8")
+class SendMessage:
+    def __init__(self, arg_message):
+        self.message = str(arg_message)
+        self.your_jid = settings_file.users['system']['secsys']
+        self.your_password = getpass()
+        self.target_jid = ''
 
-        handler = MyHandler(JID(target_jid), message)
-        settings = XMPPSettings({
-                        u"password": your_password,
-                        u"starttls": True,
-                        u"tls_verify_peer": False,
-                    })
+        self.submit()
 
-        client = Client(JID(your_jid), [handler], settings)
-        client.connect()
-        client.run()
+    def submit(self):
+        for admin in settings_file.users['administrators']:
+            self.target_jid = settings_file.users['administrators'][admin]
+            if sys.version_info.major < 3:
+                self.your_jid = self.your_jid.decode("utf-8")
+                self.your_password = self.your_password.decode("utf-8")
+                self.target_jid = self.target_jid.decode("utf-8")
+                self.message = self.message.decode("utf-8")
+
+                handler = MyHandler(JID(self.target_jid), self.message)
+                settings = XMPPSettings({
+                    u"password": self.your_password,
+                    u"starttls": True,
+                    u"tls_verify_peer": False,
+                })
+
+                client = Client(JID(self.your_jid), [handler], settings)
+                client.connect()
+                client.run()
